4b435d989c556306e10d110f00adbf41e9de4cba
Egor Red <egor.red02@gmail.com>
2023-01-18 11:37:49 +0300

    Add: WriteClassInTXT

---
+++ cs/stream_and_file/WriteClassInFileTXT/Coord3D.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace WriteClassInFileTXT
+{
+    public class Coord3D
+    {
+        public Coord3D()
+        {
+            coord_X = 0;
+            coord_Y = 0;
+            coord_Z = 0;
+        }
+
+        public Coord3D(double coord_X, double coord_Y, double coord_Z)
+        {
+            this.coord_X = coord_X;
+            this.coord_Y = coord_Y;
+            this.coord_Z = coord_Z;
+        }
+
+        public double coord_X { get; set; }
+        public double coord_Y { get; set; }
+        public double coord_Z { get; set; }
+
+        public override string ToString()
+        {
+            return $"X:{coord_X}\t Y:{coord_Y}\t Z:{coord_Z}";
+            
+        }
+
+        public void PrintCord()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public static Coord3D Parse(string coords)
+        {
+            string result = String.Concat(coords.Split(new Char[] { ':', 'X', 'Y', 'Z' }));
+            double[] parseCord = result.Split(new Char[] {' '}).Select(double.Parse).ToArray();
+            Coord3D coord3D = new Coord3D(parseCord[0], parseCord[1], parseCord[2]);
+            return coord3D;
+        }
+    }
+}
---
+++ cs/stream_and_file/WriteClassInFileTXT/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WriteClassInFileTXT
+{
+    class Program
+    {
+
+        public static Coord3D RandomCoordsValue()
+        {
+            Random randomCords = new Random();
+            Coord3D coord3D = new Coord3D();
+            coord3D.coord_X = randomCords.Next(0, 100) / 10.0;
+            coord3D.coord_Y = randomCords.Next(0, 100) / 10.0;
+            coord3D.coord_Z = randomCords.Next(0, 100) / 10.0;
+            return coord3D;
+        }
+
+        static void Main(string[] args)
+        {
+
+            List<Coord3D> coords = new List<Coord3D>();
+
+
+            string fileName = "Cords.txt";
+            using (var sw = new StreamWriter(fileName, false, Encoding.UTF8))
+            {
+                for (int i = 0; i < 9; i++)
+                {
+
+                    sw.WriteLine(RandomCoordsValue().ToString());
+                }
+            }
+
+            using (var sw = new StreamReader(fileName, Encoding.UTF8))
+            {
+                while(!sw.EndOfStream)
+                {
+                    coords.Add(Coord3D.Parse(sw.ReadLine()));
+                }
+            }
+
+
+            foreach (var item in coords)
+            {
+                item.PrintCord();
+            }
+        }
+    }
+}
