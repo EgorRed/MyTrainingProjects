7b31fd694f1850ab9e0015fc998d5dc5d368aa21
Egor Red <egor.red02@gmail.com>
2023-01-24 22:54:41 +0300

    update ClinentTCP

--- cs/ServerTCP/ClientTCP/Program.cs
+++ cs/ServerTCP/ClientTCP/Program.cs
@@ -12,40 +12,42 @@ namespace ClientTCP
             const string ip = "127.0.0.1";
             const int port = 8080;
 
-            //create endpoint
-            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
-            //create socket
-            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            Console.Write("Client: ");
-            string message = Console.ReadLine();
-
-            //encoding data
-            var data = Encoding.UTF8.GetBytes(message);
-            //connect to end point
-            tcpSocket.Connect(tcpEndPoint);
-            //send data to server 
-            tcpSocket.Send(data);
-
-            var buffer = new byte[256]; //buffer for data
-            int size = 0; //size bubber
-            var answer = new StringBuilder(); //data builder
-
-            //waiting for a response from the server
-            do
+            while (true)
             {
-                size = tcpSocket.Receive(buffer);
-                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                //create endpoint
+                var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+                //create socket
+                var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+                Console.Write("Client: ");
+                string message = Console.ReadLine();
+
+                //encoding data
+                var data = Encoding.UTF8.GetBytes(message);
+                //connect to end point
+                tcpSocket.Connect(tcpEndPoint);
+                //send data to server 
+                tcpSocket.Send(data);
+
+                var buffer = new byte[256]; //buffer for data
+                int size = 0; //size bubber
+                var answer = new StringBuilder(); //data builder
+
+                //waiting for a response from the server
+                do
+                {
+                    size = tcpSocket.Receive(buffer);
+                    answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                }
+                while (tcpSocket.Available > 0);
+
+                Console.WriteLine(answer);
+
+                //socket close
+                tcpSocket.Shutdown(SocketShutdown.Both);
+                tcpSocket.Close();
             }
-            while (tcpSocket.Available > 0);
-
-            Console.WriteLine(answer);
-
-            //socket close
-            tcpSocket.Shutdown(SocketShutdown.Both);
-            tcpSocket.Close();
-
-            Console.ReadLine();
         }
     }
 }
